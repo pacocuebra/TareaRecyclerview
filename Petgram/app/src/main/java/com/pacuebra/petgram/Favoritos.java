a90e1d424d0d26b4c3f30984bac1ac30a15b6633
pacocuebra <pacocuevon@gmail.com>
2016-06-21 12:35:14 -0500

    RecyclerView Mejorado pero aun con detalles

---
+++ Petgram/app/src/main/java/com/pacuebra/petgram/Favoritos.java
@@ -0,0 +1,15 @@
+package com.pacuebra.petgram;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Favoritos extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_favoritos);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+}
--- Petgram/app/src/main/java/com/pacuebra/petgram/MainActivity.java
+++ Petgram/app/src/main/java/com/pacuebra/petgram/MainActivity.java
@@ -1,25 +1,35 @@
 package com.pacuebra.petgram;
 
+import android.content.ClipData;
 import android.content.Intent;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.LinearLayout;
 import android.widget.Toast;
 
+
 import java.util.ArrayList;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity  {
     private RecyclerView listaPerros;
     //ImageButton bthueso1;
     ArrayList<Perro> contactos;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        ActionBar actionbar = getSupportActionBar();
+        actionbar.setDisplayShowHomeEnabled(true);
+        //actionbar.setIcon(R.drawable.star_50);
         setContentView(R.layout.activity_main);
+
 
         listaPerros = (RecyclerView)findViewById(R.id.rvPerros);
 
@@ -53,6 +63,13 @@ public void inciarAdaptador(){
     listaPerros.setAdapter(adaptador);
 }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+                menuInflater.inflate(R.menu.favoritos, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
     public void irActividadDos (View v){
         //Perro contacto = contactos.get(position);
         Intent intent = new Intent(this,Main2Activity.class);
@@ -63,6 +80,17 @@ public void inciarAdaptador(){
         startActivity(intent);
     }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.action_favorite:
+                Intent intent2 = new Intent(this,Favoritos.class);
+                startActivity(intent2);
+                Toast.makeText(getApplicationContext(),"Favoritos",Toast.LENGTH_SHORT).show();
+                default:
+                    return super.onOptionsItemSelected(item);
+        }
 
+    }
 }
 
